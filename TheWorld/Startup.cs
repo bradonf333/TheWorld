bf458e341ad6cccd211200774f5952e1e0445f8d
bradonf333 <bradonf123@gmail.com>
2017-03-17 23:32:01 -0600

    Added comments

--- TheWorld/Startup.cs
+++ TheWorld/Startup.cs
@@ -21,7 +21,9 @@ namespace TheWorld
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            // Changes the default files request
             app.UseDefaultFiles();
+            // Static files finds the files
             app.UseStaticFiles();
         }
     }
