8a1784838738eb6acb933fec561956531e779466
bradonf333 <bradonf123@gmail.com>
2017-03-18 21:21:13 -0600

    Commented out previous jQuery code. Used only for testing/learning

--- TheWorld/wwwroot/scripts/site.js
+++ TheWorld/wwwroot/scripts/site.js
@@ -1,21 +1,24 @@
 ﻿// Self executing anonymous function or an immediately invoked function expression
 (function () {
 
-    var ele = $("#username");
-    ele.text("Bradon Fredrickson");
+    //var ele = $("#username");
+    //ele.text("Bradon Fredrickson");
 
-    var main = $("#main");
-    main.on("mouseenter", function () {
-        main.style = "background-color: #888;";
-    });
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    main.style = "background-color: #888;";
+    //});
 
-    main.on("mouseleave", function () {
-        main.style = "";
-    });
+    //main.on("mouseleave", function () {
+    //    main.style = "";
+    //});
 
-    var menuItems = $("ul.menu li a");
-    menuItems.on("click", function () {
-        var me = $(this);
-        alert(me.text());
-    });
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
 })();
