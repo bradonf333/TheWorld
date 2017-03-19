5deacbb1d932f20d098f814ad7e020ced8818307
bradonf333 <bradonf123@gmail.com>
2017-03-18 20:44:40 -0600

    Added onmouseenter and leave events for the main id

--- TheWorld/wwwroot/scripts/site.js
+++ TheWorld/wwwroot/scripts/site.js
@@ -1,3 +1,12 @@
 ﻿
 var ele = document.getElementById("username");
 ele.innerHTML = "Bradon Fredrickson";
+
+var main = document.getElementById("main");
+main.onmouseenter = function () {
+    main.style.backgroundColor = "#888";
+};
+
+main.onmouseleave = function () {
+    main.style.backgroundColor = "";
+};
