389d59943c20704b5ac054f2e6ade959b029b818
bradonf333 <bradonf123@gmail.com>
2017-03-18 20:52:24 -0600

    Put js code into a self exxecuting anonymous function

--- TheWorld/wwwroot/scripts/site.js
+++ TheWorld/wwwroot/scripts/site.js
@@ -1,12 +1,14 @@
-﻿
-var ele = document.getElementById("username");
-ele.innerHTML = "Bradon Fredrickson";
+﻿// Self executing anonymous function or an immediately invoked function expression
+(function () {
+    var ele = document.getElementById("username");
+    ele.innerHTML = "Bradon Fredrickson";
 
-var main = document.getElementById("main");
-main.onmouseenter = function () {
-    main.style.backgroundColor = "#888";
-};
+    var main = document.getElementById("main");
+    main.onmouseenter = function () {
+        main.style.backgroundColor = "#888";
+    };
 
-main.onmouseleave = function () {
-    main.style.backgroundColor = "";
-};
+    main.onmouseleave = function () {
+        main.style.backgroundColor = "";
+    };
+})();
