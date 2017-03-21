966926a57f6f3fbbe393928ddd8a381c081a023e
bradonf333 <bradonf123@gmail.com>
2017-03-20 21:26:55 -0600

    clear the contact page when message is sent

--- TheWorld/Controllers/Web/AppController.cs
+++ TheWorld/Controllers/Web/AppController.cs
@@ -51,6 +51,8 @@ namespace TheWorld.Controllers.Web
             {
                 _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "From TheWorld", model.Message);
 
+                // Clears the contact fields and lets user know message was sent
+                ModelState.Clear();
                 ViewBag.UserMessage = "Message Sent";
             }
             return View();
