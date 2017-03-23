1638ece8d5d7844fda7efb5a3ec31c3cd84902f6
bradonf333 <bradonf123@gmail.com>
2017-03-22 20:25:28 -0600

    Created class to represent a stop(destination) in the map

--- TheWorld/Models/Stop.cs
+++ TheWorld/Models/Stop.cs
@@ -1,6 +1,15 @@
-﻿namespace TheWorld.Models
+﻿using System;
+
+namespace TheWorld.Models
 {
     public class Stop
     {
+        public int Id { get; set; }
+        public string Name { get; set; } // City
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public int Order { get; set; }
+        public DateTime Arrival { get; set; }
+
     }
 }
