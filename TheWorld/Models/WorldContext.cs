81b015715ef6ef010ddada0f345bc1991920c3d1
bradonf333 <bradonf123@gmail.com>
2017-03-22 20:54:31 -0600

    Added Microsoft.EntityFrameworkCore using Nuget

---
+++ TheWorld/Models/WorldContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TheWorld.Models
+{
+    public class WorldContext : DbContext
+    {
+        
+    }
+}
