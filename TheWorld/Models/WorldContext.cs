c6c5a7d6e3c1c1339b4ba9e2d3b1d861317c4bd6
bradonf333 <bradonf123@gmail.com>
2017-03-22 20:55:34 -0600

    Added DbSets for Trip and Stop

--- TheWorld/Models/WorldContext.cs
+++ TheWorld/Models/WorldContext.cs
@@ -9,6 +9,12 @@ namespace TheWorld.Models
 {
     public class WorldContext : DbContext
     {
-        
+        public WorldContext()
+        {
+
+        }
+
+        public DbSet<Trip> Trips { get; set; }
+        public DbSet<Stop> Stops { get; set; }
     }
 }
