ea712a85243b458a574ee8bc96605ea3c938044a
bradonf333 <bradonf123@gmail.com>
2017-03-23 18:59:15 -0600

    Got the sql database created

---
+++ TheWorld/masterContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+
+namespace TheWorld
+{
+    public partial class masterContext : DbContext
+    {
+        public IConfigurationRoot _config { get; private set; }
+
+        /// <summary>
+        /// Can use the configuration information that is created in Startup.cs
+        /// </summary>
+        /// <param name="config"></param>
+        public masterContext(IConfigurationRoot config, DbContextOptions options) : base()
+        {
+            _config = config;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_config["ConnectionStrings:WorldContextConnection"]);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
