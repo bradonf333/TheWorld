b9d456e7ad56e7baf547c46f56688902d7b7730e
bradonf333 <bradonf123@gmail.com>
2017-03-23 21:46:54 -0600

    WorldContextSeeData will create data in our new database

---
+++ TheWorld/Models/WorldContextSeedData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWorld.Models
+{
+    public class WorldContextSeedData
+    {
+        private WorldContext _context;
+
+        /// <summary>
+        /// WorldContext is where we have our optionsBuilder.UseSqlServer & our connection string
+        /// </summary>
+        /// <param name="context"></param>
+        public WorldContextSeedData(WorldContext context)
+        {
+            _context = context;
+        }
+
+        public async Task EnsureSeedData()
+        {
+
+        }
+    }
+}
