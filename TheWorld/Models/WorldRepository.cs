1ec5273a7b13b9f23923300bea84efecd0ebe6a7
bradonf333 <bradonf123@gmail.com>
2017-03-25 07:53:35 -0600

    Use repo to run query

---
+++ TheWorld/Models/WorldRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWorld.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class WorldRepository
+    {
+        private WorldContext _context;
+
+        public WorldRepository(WorldContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Gets all the trips from the database
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Trip> GetAllTrips()
+        {
+            // Can think of this as a query
+            return _context.Trips.ToList();
+        }
+    }
+}
