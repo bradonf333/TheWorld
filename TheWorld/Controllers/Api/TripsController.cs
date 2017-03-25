3a047059488db8b7daebb1d6b4e817983757187b
bradonf333 <bradonf123@gmail.com>
2017-03-25 09:35:28 -0600

    Api returns a new trip object as JSON

--- TheWorld/Controllers/Api/TripsController.cs
+++ TheWorld/Controllers/Api/TripsController.cs
@@ -4,11 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheWorld.Models;
 
 namespace TheWorld.Controllers.Api
 {
     public class TripsController : Controller
     {
-        
+        public JsonResult Get()
+        {
+            return Json(new Trip() {Name = "My Trip" });
+        }
     }
 }
