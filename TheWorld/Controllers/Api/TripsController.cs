59f58ff74b989e629617ec4695d53c1fcfcfc457
bradonf333 <bradonf123@gmail.com>
2017-03-25 12:06:34 -0600

    Changed to IActionResult so we can handle any bad requests or errors

--- TheWorld/Controllers/Api/TripsController.cs
+++ TheWorld/Controllers/Api/TripsController.cs
@@ -10,15 +10,46 @@ namespace TheWorld.Controllers.Api
 {
     public class TripsController : Controller
     {
+        /* 
+         * ===============================================================
+         *      -- Return the Api as JSON
+         *      
+         *  This is how we set it up beforehand, but switched to below.
+         *  -Wanted to keep this as an example.
+         *  
+         *  Using JSON we can't really return any errors.
+         * ===============================================================
+         */
+        /*
+       // When this url is called it will then call this method
+       /// <summary>
+       /// Serializes as Json file
+       /// </summary>
+       /// <returns>Json</returns>
+       [HttpGet("api/trips")]
+       public JsonResult Get()
+       {
+           return Json(new Trip() {Name = "My Trip" });
+       }
+       */
+
+        /* 
+         * ================================================================
+         *  IActionResult we can return a response code. Ok is for success
+         * 
+         * -Using this still returns JSON in the Api.
+         *  -If an error happens we can handle that here.
+         * ================================================================
+         */
         // When this url is called it will then call this method
         /// <summary>
         /// Serializes as Json file
         /// </summary>
         /// <returns>Json</returns>
         [HttpGet("api/trips")]
-        public JsonResult Get()
+        public IActionResult Get()
         {
-            return Json(new Trip() {Name = "My Trip" });
+            return Ok(new Trip() { Name = "My Trip" });
         }
     }
 }
