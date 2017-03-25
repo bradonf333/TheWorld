5a5bbe3a41714bcc3356979abffe039c6f51daad
bradonf333 <bradonf123@gmail.com>
2017-03-25 12:14:55 -0600

    added json option to make sure it is returned in camelCase

--- TheWorld/Startup.cs
+++ TheWorld/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Logging;
 using TheWorld.Services;
 using Microsoft.Extensions.Configuration;
 using TheWorld.Models;
+using Newtonsoft.Json.Serialization;
 
 namespace TheWorld
 {
@@ -73,7 +74,12 @@ namespace TheWorld
             // Makes it so we can log exceptions
             services.AddLogging();
 
-            services.AddMvc();
+            services.AddMvc()
+                // Makes sure our JSON is returned in camelCase
+                .AddJsonOptions(config =>
+                {
+                    config.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
