9e1ac01ca4487005f0fd16859f0eedfe57708617
bradonf333 <bradonf123@gmail.com>
2017-04-18 20:41:00 -0600

    Implement method to get the trip name for the given stop

--- TheWorld/Controllers/Api/StopsController.cs
+++ TheWorld/Controllers/Api/StopsController.cs
@@ -14,10 +14,29 @@ namespace TheWorld.Controllers.Api
         private ILogger<StopsController> _logger;
         private IWorldRepository _repository;
 
+        /// <summary>
+        /// Constructor to initialize local variables needed
+        /// </summary>
+        /// <param name="repository"></param>
+        /// <param name="logger"></param>
         public StopsController(IWorldRepository repository, ILogger<StopsController> logger)
         {
             _repository = repository;
             _logger = logger;
+        }
+
+
+        /*
+         * ==============================================
+         *   -- Return the stops for a specific trip --
+         * ==============================================
+         */
+        public IActionResult Get()
+        {
+            // Since we are dealing with a Stop we need the trip name
+            var trip = _repository.GetTripByName(string tripName);
+
+            return Ok(trip.Stops);
         }
     }
 }
