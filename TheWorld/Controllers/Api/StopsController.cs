25641c3d9caaa4f3cf6acd9fafe4fc9e14f6c6c4
bradonf333 <bradonf123@gmail.com>
2017-04-18 20:38:46 -0600

    Added ctor for Stops controller

---
+++ TheWorld/Controllers/Api/StopsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheWorld.Models;
+
+namespace TheWorld.Controllers.Api
+{
+    public class StopsController : Controller
+    {
+        private ILogger<StopsController> _logger;
+        private IWorldRepository _repository;
+
+        public StopsController(IWorldRepository repository, ILogger<StopsController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+    }
+}
