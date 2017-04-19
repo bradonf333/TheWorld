91d7f911780944be198901649a46da587b6e93a5
bradonf333 <bradonf123@gmail.com>
2017-04-18 20:55:18 -0600

    Order and Arrival are required. Lat and Long are not

--- TheWorld/ViewModel/StopViewModel.cs
+++ TheWorld/ViewModel/StopViewModel.cs
@@ -12,9 +12,14 @@ namespace TheWorld.ViewModel
         [Required]
         [StringLength(100, MinimumLength =5)]
         public string Name { get; set; } // City
+
         public double Latitude { get; set; }
         public double Longitude { get; set; }
+
+        [Required]
         public int Order { get; set; }
+
+        [Required]
         public DateTime Arrival { get; set; }
     }
 }
