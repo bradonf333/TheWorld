efb8d2e132b658b0f5fb6e8755b8b2b7003a5915
bradonf333 <bradonf123@gmail.com>
2017-04-19 20:34:27 -0600

    created properties

--- TheWorld/Services/GeoCoordsResult.cs
+++ TheWorld/Services/GeoCoordsResult.cs
@@ -2,5 +2,9 @@
 {
     public class GeoCoordsResult
     {
+        public bool Success { get; set; }       // Whether it was successful or not
+        public string Message { get; set; }     // String for any messages we need to relay
+        public double Longitude { get; set; }   // Location
+        public double Latitude { get; set; }    // Location
     }
 }
