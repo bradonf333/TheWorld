d835396a7b54c9d95576e0fcb5fd014ccc9b7f1d
bradonf333 <bradonf123@gmail.com>
2017-04-19 20:29:33 -0600

    Add logger to our method

--- TheWorld/Services/GeoCoordsService.cs
+++ TheWorld/Services/GeoCoordsService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ namespace TheWorld.Services
 {
     public class GeoCoordsService
     {
-        public GeoCoordsService()
+        public GeoCoordsService(ILogger<GeoCoordsService> logger)
         {
 
         }
