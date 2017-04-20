78421a337e82d8a65efdbbceea1bce3a28046b98
bradonf333 <bradonf123@gmail.com>
2017-04-19 21:04:46 -0600

    Added code included in the resource file

--- TheWorld/Services/GeoCoordsService.cs
+++ TheWorld/Services/GeoCoordsService.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -35,6 +37,32 @@ namespace TheWorld.Services
             var apiKey = _config["Keys:BingKey"]; // Access our BingKey
             var encodedName = WebUtility.UrlEncode(name);
             var url = $"http://dev.virtualearth.net/REST/v1/Locations?q={encodedName}&key={apiKey}";
+
+            var client = new HttpClient();
+
+            var json = await client.GetStringAsync(url);
+
+            // Read out the results
+            // Fragile, might need to change if the Bing API changes
+            var results = JObject.Parse(json);
+            var resources = results["resourceSets"][0]["resources"];
+            if (!results["resourceSets"][0]["resources"].HasValues)
+            {
+                result.Message = $"Could not find '{name}' as a location";
+            }
+            else
+            {
+                var confidence = (string)resources[0]["confidence"];
+                if (confidence != "High")
+                {
+                    result.Message = $"Could not find a confident match for '{name}' as a location";
+                }
+                else
+                {
+                    var coords = resources[0]["geocodePoints"][0]["coordinates"];
+
+                }
+            }
         }
         
     }
